7006bce4539817dc353c82fb3764d9432a51a938
Juha Vierinen <jvierine@gmail.com>
2020-08-20 20:23:30 +0000

    added mandelbrot

--- 001_hello_world/mystery.py
+++ 001_hello_world/mystery.py
@@ -3,15 +3,18 @@ import numpy as n
 import scipy.constants as c
 import matplotlib.pyplot as plt
 
-# test some functions
-x=n.linspace(-2,2,num=2000)
-cx,cy=n.meshgrid(x,x)
+# test some complex numpy functions
+x=n.linspace(-2.5,1.5,num=2000)
+y=n.linspace(-2,2,num=2000)
+cx,cy=n.meshgrid(x,y)
 c=cx+1j*cy
 z=n.zeros(c.shape,dtype=n.complex64)
-for n_iterations in range(100):
+for iteration in range(12):
     z=z**2+c
-
-plt.imshow(n.abs(z),extent=[-2,2,-2,2],vmin=0,vmax=1,cmap="plasma")
+z[n.isnan(z)]=0.0
+z[n.isinf(z)]=0.0
+# test plotting the phase of a complex number
+plt.imshow(n.angle(z),extent=[-2,2,-2,2],cmap="hsv")
 plt.colorbar()
 plt.xlabel("$\mathrm{Re}\{z\}$")
 plt.ylabel("$\mathrm{Im}\{z\}$")
