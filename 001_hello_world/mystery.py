4f4e7ca7a71c8cd0bfdb1bb6e8442a7c0680d3ac
juha <jvierine@gmail.com>
2020-08-28 22:14:53 +0000

    fixed a bug. the datatype of variable c wasn't complex64 but complex128

--- 001_hello_world/mystery.py
+++ 001_hello_world/mystery.py
@@ -3,10 +3,15 @@ import numpy as n
 import scipy.constants as c
 import matplotlib.pyplot as plt
 
-x=n.linspace(-2.5,1.5,num=2000)
-y=n.linspace(-2,2,num=2000)
+x=n.linspace(-2.5,1.5,num=2000,dtype=n.float32)
+y=n.linspace(-2,2,num=2000,dtype=n.float32)
 cx,cy=n.meshgrid(x,y)
 c=cx+1j*cy
+
+# figure out what is the datatype of variable c
+# to make sure it's complex64
+print(c.dtype)
+
 z=n.zeros(c.shape,dtype=n.complex64)
 
 for iteration in range(12):
