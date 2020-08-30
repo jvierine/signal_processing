fc6f398a229fec3f679e435f2bc28d2ba99d308a
juha <jvierine@gmail.com>
2020-08-30 11:38:08 +0000

    added a plot of a spiral using complex numbers

---
+++ 006_spiral/spiral.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as n
+
+m=n.arange(21)
+
+theta=2.0*n.pi/10.0
+
+z=0.9*n.exp(1j*theta)
+
+plt.plot(n.real(z**m),n.imag(z**m),".-",label="$0.9^n \mathrm{exp}\left(i \\frac{2\pi}{20}\\right)^n$")
+
+z=1.1*n.exp(1j*theta)
+plt.plot(n.real(z**m),n.imag(z**m),".-",label="$1.1^n \mathrm{exp}\left(i \\frac{2\pi}{20}\\right)^n$")
+
+plt.plot(n.cos(n.linspace(0,2*n.pi,num=1000)),n.sin(n.linspace(0,2*n.pi,num=1000)),color="grey",label="Unit circle $e^{i\\theta}$")
+plt.legend()
+plt.xlabel("Real axis")
+plt.ylabel("Imaginary axis")
+plt.axes().set_aspect('equal')
+plt.tight_layout()
+plt.savefig("spiral.png")
+plt.show()
+
