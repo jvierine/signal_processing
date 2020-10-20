60cf99ac67fd2ddecc0c02a874716fea318e9527
Juha Vierinen <jvierine@gmail.com>
2020-10-20 21:59:59 +0000

    added filter design example

---
+++ 026_iir/iir_design.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+import numpy as n
+import scipy.signal as s
+
+import plot_h
+
+# Design an IIR low pass filter with:
+# sample-rate = 1000 Hz
+# edge of the band pass = 100 Hz
+# start of the band stop = 200 Hz
+sample_rate=1000.0
+zeros,poles,k=s.iirdesign(100.0, 200.0, gpass=1.0, gstop=100, ftype='ellip', output="zpk",fs=sample_rate)
+
+# return the filter coefficients for the IIR filter
+b,a=s.iirdesign(100.0, 200.0, gpass=1.0, gstop=100, ftype='ellip', output="ba",fs=sample_rate)
+
+plot_h.plot_hmag("ex_design.png",zeros=zeros,poles=poles,vmin=-150,fs=sample_rate)
