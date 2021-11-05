47cbf0c2f2340c595c50d84b5a9165f1accca6b8
Juha Vierinen <jvierine@gmail.com>
2021-11-05 16:44:28 +0000

    added ligo data and plot

---
+++ 029_ligo/plot_ligo.py
@@ -0,0 +1,21 @@
+import h5py
+import matplotlib.pyplot as plt
+
+def read_data(fname):
+    h=h5py.File(fname,"r")
+    detector_name=h["meta/Detector"][()]
+    start_time = h["meta/UTCstart"][()]
+    strain=h["strain/Strain"][()]
+    print("Reading %s data starting at %s"%(str(detector_name),str(start_time)))
+    h.close()
+    return(detector_name,start_time,strain)
+
+# read hanford measurement
+h1_name,h1_start_time,h1_strain=read_data("H-H1_LOSC_4_V2-1126259446-32.hdf5")
+
+# read livingston measurement
+l1_name,l1_start_time,l1_strain=read_data("L-L1_LOSC_4_V2-1126259446-32.hdf5")
+    
+plt.plot(h1_strain)
+plt.plot(l1_strain)
+plt.show()
