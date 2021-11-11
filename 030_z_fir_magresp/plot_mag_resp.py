f1b0f6c97eb981031cc9436bbfc2e2ed697ff21b
Juha Vierinen <jvierine@gmail.com>
2021-11-11 10:01:43 +0000

    program

---
+++ 030_z_fir_magresp/plot_mag_resp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import numpy as n
+import matplotlib.pyplot as plt
+
+# frequency, in units of radians per sample
+omhat = n.linspace(-n.pi,n.pi,num=100000)
+
+def h50_100(z):
+    om0=2.0*n.pi*50.0/(44.1e3)
+    om1=2.0*n.pi*1000.0/(44.1e3)
+    print(om0)
+    print(om1)    
+    return(z**(-4)*( z**4 - 2*z**3*(n.cos(om1)+n.cos(om0))
+                     + 2*z**2*(1+2*n.cos(om1)*n.cos(om0))
+                     - 2*z*(n.cos(om0)+n.cos(om1)) + 1))
+
+
+magresp = n.abs(h50_100(n.exp(1j*omhat)))
+plt.plot(omhat,10.0*n.log10(magresp**2.0))
+plt.xlabel("Frequency $\hat{\omega}$ (rad/sample)")
+plt.ylabel("Magnitude response $|\mathcal{H}(\hat{\omega})|$")
+plt.show()
