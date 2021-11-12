fc093855600c2eae7d121d25e2d62f6b3004a3ca
Juha Vierinen <jvierine@gmail.com>
2021-11-12 10:54:45 +0000

    ad

--- 030_z_fir_magresp/plot_mag_resp.py
+++ 030_z_fir_magresp/plot_mag_resp.py
@@ -6,14 +6,17 @@ import matplotlib.pyplot as plt
 # frequency, in units of radians per sample
 omhat = n.linspace(-n.pi,n.pi,num=100000)
 
-def h50_100(z):
+# system function
+def h(z):
     om0=2.0*n.pi*5000.0/(44.1e3)
     om1=2.0*n.pi*1500.0/(44.1e3)
     return(z**(-4)*( z**4 - 2*z**3*(n.cos(om1)+n.cos(om0))
                      + 2*z**2*(1+2*n.cos(om1)*n.cos(om0))
                      - 2*z*(n.cos(om0)+n.cos(om1)) + 1))
 
-magresp = n.abs(h50_100(n.exp(1j*omhat)))
+# magnitude response
+magresp = n.abs(h(n.exp(1j*omhat)))
+# plot the magnitude response in dB scale
 plt.plot(omhat,10.0*n.log10(magresp**2.0))
 plt.xlabel("Frequency $\hat{\omega}$ (rad/sample)")
 plt.ylabel("Magnitude response $10\log_{10}(|\mathcal{H}(\hat{\omega})|^2)$ dB")
